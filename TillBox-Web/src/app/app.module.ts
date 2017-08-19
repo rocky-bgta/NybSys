5cd3952a63c82efb237418e0a6f6a5faed9973de
rocky-bgta <rocky.bgta@gmail.com>
2017-08-19 14:43:18 +0600

    Add index.ts file to export multiple components by single import statement

--- TillBox-Web/src/app/app.module.ts
+++ TillBox-Web/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+//import { LayoutComponent } from './layout/layout.component';
+//import { HeaderComponent } from './layout/header/header.component';
+import { LayoutComponent, HeaderComponent } from './layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ TillBox-Web/src/app/layout/index.ts
@@ -0,0 +1,2 @@
+export { LayoutComponent } from "./layout.component";
+export { HeaderComponent } from "../layout/header/header.component";
